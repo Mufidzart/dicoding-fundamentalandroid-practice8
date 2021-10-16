7cf48dbb7d3ad6157f91d47137adcc385933184e
Mufidz <mufidz.xtav2.05@gmail.com>
2021-10-16 14:02:44 +0700

    practice8

---
+++ app/src/main/java/com/example/mufidzpractice8/ListHeroAdapter.java
@@ -0,0 +1,6 @@
+package com.example.mufidzpractice8;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> {
+}
